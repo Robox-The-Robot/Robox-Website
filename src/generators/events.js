e5d7a5439d807c7f586a509edd7525cfbbd456c6
Cowbiler <sebgtaylor@gmail.com>
2024-03-24 21:38:26 +1100

    initial commit

---
+++ src/generators/events.js
@@ -0,0 +1,8 @@
+import * as Blockly from 'blockly/core';
+import { pythonGenerator } from 'blockly/python'
+
+pythonGenerator.forBlock['event_begin'] = function (block, generator) {
+    var event_code = generator.statementToCode(block, 'event_code');
+    var code = `def event_begin():\n ${event_code}`;
+    return code;
+};
