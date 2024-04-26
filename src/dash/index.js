9425464a482258d6fd5e613904e9b0f2dbfd97f0
Cowbiler <sebgtaylor@gmail.com>
2024-04-26 14:36:11 +1000

    created the dashboard frontend

--- src/dash/index.js
+++ src/dash/index.js
@@ -1,2 +1,8 @@
 import * as Blockly from 'blockly/core';
 import "./index.css"
+import "../colorvars.css"
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
