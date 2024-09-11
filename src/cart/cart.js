27c4b48e661cf239a9f26ad0c82adcbfffb28386
Cowbilet <sebgtaylor@gmail.com>
2024-09-11 14:15:38 +1000

    started the cart page

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -2,4 +2,10 @@ import "../cross.webp"
 
 import "../root.css"
 import "./cart.css"
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
 
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash"
+
+
+library.add(faTrash);
+dom.watch()
