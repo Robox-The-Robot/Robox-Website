66cc171cae2cf664f39eae4aab7462504a16fe56
Yuma Soerianto <hi@madebyyuma.com>
2025-02-10 23:57:25 +1100

    Fixed top-level awaits in dev and prod configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,11 @@ import { merge } from 'webpack-merge'
 
 import common from "./webpack.config.js"
 
-let config = await common()
-
-export default merge(config, {
-    mode: 'development',
-    devtool: false,
-});
+export default function() {
+    return common().then((config) => {
+        return merge(config, {
+            mode: 'development',
+            devtool: false,
+        });
+    });
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,60 +2,63 @@ import { merge } from 'webpack-merge'
 
 import common from "./webpack.config.js"
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin"
-import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin'
+// import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin'
 import CompressionPlugin from "compression-webpack-plugin"
 
-let config = await common()
-const mergedConfig = merge(config, {
-    mode: 'production',
-    devtool: false,
-    plugins: [
-        new CompressionPlugin({
-            test: /\.js(\?.*)?$/i,
-        })
-    ],
-    optimization: {
-        minimizer: [
-            `...`,
-            new CssMinimizerPlugin(),
-            
-        ],
-    },
-});
-mergedConfig.module.rules = [
-    {
-        test: /\.css$/i,
-        use: ['css-loader'], //"style-loader"
-    },
-    {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-    },
-    {
-        test: /\.(ico|png|jp?g|svg)/,
-        oneOf: [
-            // inline image using `?inline` query
+export default function() {
+    return common().then((config) => {
+        const mergedConfig = merge(config, {
+            mode: 'production',
+            devtool: false,
+            plugins: [
+                new CompressionPlugin({
+                    test: /\.js(\?.*)?$/i,
+                })
+            ],
+            optimization: {
+                minimizer: [
+                    `...`,
+                    new CssMinimizerPlugin(),
+                    
+                ],
+            },
+        });
+        mergedConfig.module.rules = [
             {
-                resourceQuery: /inline/,
-                type: 'asset/inline',
+                test: /\.css$/i,
+                use: ['css-loader'], //"style-loader"
             },
             {
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                    maxSize: 1024,
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ico|png|jp?g|svg)/,
+                oneOf: [
+                    // inline image using `?inline` query
+                    {
+                        resourceQuery: /inline/,
+                        type: 'asset/inline',
                     },
-                },
-                generator: {
-                    filename:({ filename }) => {
-                        let splitname = filename.split("/")
-                        if (splitname[splitname.length-3] === "product") return `public/images/${splitname[splitname.length-2]}/[name][ext][query]`
-                        else return "public/images/[hash][ext][query]"
-                    }
-                }
+                    {
+                        type: 'asset',
+                        parser: {
+                            dataUrlCondition: {
+                            maxSize: 1024,
+                            },
+                        },
+                        generator: {
+                            filename:({ filename }) => {
+                                let splitname = filename.split("/")
+                                if (splitname[splitname.length-3] === "product") return `public/images/${splitname[splitname.length-2]}/[name][ext][query]`
+                                else return "public/images/[hash][ext][query]"
+                            }
+                        }
+                    },
+                ],
             },
-        ],
-    },
-]
-
-export default mergedConfig
+        ]
+        
+        return mergedConfig
+    })
+}
