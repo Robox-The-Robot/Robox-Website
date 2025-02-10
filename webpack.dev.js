61f6ba035dc3f1da4a6847201c136430c233b4b6
Cowbilet <sebgtaylor@gmail.com>
2025-02-10 15:35:46 +1100

    Fixed error

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,9 @@ import { merge } from 'webpack-merge'
 
 import common from "./webpack.config.js"
 
+let config = await common()
 
-
-export default merge(common, {
+export default merge(config, {
     mode: 'development',
     devtool: false,
 });
