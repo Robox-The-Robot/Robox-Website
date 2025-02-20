589a8db6050853a877284ef16787614d50a601fb
Cowbilet <sebgtaylor@gmail.com>
2025-02-20 21:13:15 +1100

    added listening to both HTTP and HTTPS port

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.use("/", express.static("./dist"))
 
 // Serve the files on port 3000.
 app.listen(80, function () {
-    console.log('Robox website listening on port 3000!\n');
+    console.log('Robox website listening on port 80!\n');
+}); 
+app.listen(443, function () {
+    console.log('Robox website listening on port 443!\n');
 }); 
