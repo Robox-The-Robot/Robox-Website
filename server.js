51ed10fd70a7413ca4386eb7edc0c7d77e935a30
Cowbilet <sebgtaylor@gmail.com>
2025-02-20 22:10:16 +1100

    fixed the port listening (sorry evan)

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ app.use("/api/store", payment);
 app.use("/", express.static("./dist"))
 
 // Serve the files on port 3000.
-app.listen(80, function () {
+app.listen(3000, function () {
     console.log('Robox website listening on port 80!\n');
-}); 
-app.listen(443, function () {
-    console.log('Robox website listening on port 443!\n');
 }); 
