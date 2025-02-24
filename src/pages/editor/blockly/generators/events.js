731d664c0e7f7ac22285ce0b31eb68f204602ab3
Cowbilet <sebgtaylor@gmail.com>
2025-02-25 10:53:11 +1100

    added global variables for motor polarity to event_begin since python is dumb

--- src/pages/editor/blockly/generators/events.js
+++ src/pages/editor/blockly/generators/events.js
@@ -3,6 +3,7 @@ import { pythonGenerator } from 'blockly/python'
 
 pythonGenerator.forBlock['event_begin'] = function (block, generator) {
     var event_code = generator.statementToCode(block, 'event_code');
-    var code = `def event_begin():\n${event_code}`;
+    var code = `def event_begin():\n
+  global left_motor_polarity, right_motor_polarity\n${event_code}`;
     return code;
 };
